d4302fddb2c42a4ff94701cf6422b1b9c73310fc
Akalchi <alejandrafdez1699@gmail.com>
2025-02-24 18:35:21 +0100

    "FEAT [findComputer] implemented to fix failing test"

--- src/main/java/dev/alejandra/controller/StoreController.java
+++ src/main/java/dev/alejandra/controller/StoreController.java
@@ -20,8 +20,15 @@ public class StoreController {
      public List<Computer> getComputers() {
         return store.getComputers();
     }
-    
+
     public void removeComputer(String brand) {
         store.getComputers().removeIf(c -> c.getBrand().equalsIgnoreCase(brand));
+    }
+
+    public Computer findComputer(String brand) {
+        return store.getComputers().stream()
+                .filter(c -> c.getBrand().equalsIgnoreCase(brand))
+                .findFirst()
+                .orElse(null);
     }
 }
