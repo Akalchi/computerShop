3ed0e554bfcafd24989f4a250059d6335c408fca
Akalchi <alejandrafdez1699@gmail.com>
2025-02-24 18:12:45 +0100

    "FEAT [Computer] implemented (attributes, constructor, getters and Computer import) to fix failing test"

--- src/test/java/dev/alejandra/tests/ComputerTest.java
+++ src/test/java/dev/alejandra/tests/ComputerTest.java
@@ -2,8 +2,8 @@ package dev.alejandra.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
+import dev.alejandra.model.Computer;
 
 public class ComputerTest {
 @Test
